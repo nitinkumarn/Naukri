72a0362e7a4fbd084885ad13177bebbb9c58c6f7
unknown <nitinkumarsuccess@gmail.com>
2025-01-29 18:27:01 +0530

    Initial commit

---
+++ tests/UIBASICS.spec.js
@@ -0,0 +1,24 @@
+const {test,expect} =require('@playwright/test');
+
+test.only('Naukri Login',async ({browser})=>
+{
+const context = await browser.newContext();
+const page = await context.newPage();
+await page.goto("https://www.naukri.com/nlogin/login");
+await page.locator('#usernameField').fill("nitinkumarsuccess@gmail.com");
+await page.locator('#passwordField').fill("Nitink@1995");
+await page.locator('button.waves-effect.waves-light.btn-large.btn-block.btn-bold.blue-btn.textTransform[type="submit"]').click();
+await page.locator('[href="/mnjuser/profile"]').click();
+await page.locator('[class="icon edit "]').click();
+await page.locator('#name').fill('Nitinkumar N');
+await page.locator('#saveBasicDetailsBtn').click();
+});
+
+test('Page First PlayWright Test',async ({page})=>
+    {
+    // const context = await browser.newContext();
+    // const page = await context.newPage();
+    await page.goto("https://google.com");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
+    });
