39229d2ce8dd7655013304baba684f3ff445dfc5
nitinkumarn <nitinkumarsuccess@gmail.com>
2025-01-30 10:17:42 +0530

    Schedule cancelled 1

--- playwright.config.js
+++ playwright.config.js
@@ -43,10 +43,10 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
     // {
     //   name: 'webkit',
