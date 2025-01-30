44fd11244fcc024cbf1e8cbef961d5d28f8c6fd4
nitinkumarn <nitinkumarsuccess@gmail.com>
2025-01-30 09:49:32 +0530

    added chrome

--- playwright.config.js
+++ playwright.config.js
@@ -38,10 +38,10 @@ module.exports = defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-    // },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
 
     {
       name: 'firefox',
