b6c57a8e3a7bb91698eae5ecf67bd8945549e250
nitinkumarn <nitinkumarsuccess@gmail.com>
2025-02-02 20:36:14 +0530

    2

--- playwright.config.js
+++ playwright.config.js
@@ -51,10 +51,10 @@ module.exports = defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
