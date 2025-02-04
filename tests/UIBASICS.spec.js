83f5d3e6d3f60400cf74fd1858a6c854c3261fe8
nitinkumarn <nitinkumarsuccess@gmail.com>
2025-02-04 10:45:08 +0530

    Added Ds Nau profile

--- tests/UIBASICS.spec.js
+++ tests/UIBASICS.spec.js
@@ -24,3 +24,18 @@ test('Page First PlayWright Test',async ({page})=>
     console.log(await page.title());
     await expect(page).toHaveTitle("Google");
     });
+
+    test('Ds Naukri Login',async ({browser})=>
+        {
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto("https://www.naukri.com/nlogin/login");
+        await page.locator('#usernameField').fill("deepikabu1998@gmail.com");
+        await page.locator('#passwordField').fill("DeepikaUmesh@098");
+        await page.locator('button.waves-effect.waves-light.btn-large.btn-block.btn-bold.blue-btn.textTransform[type="submit"]').click();
+        await page.locator('[href="/mnjuser/profile"]').click();
+        await page.locator('[class="icon edit "]').click();
+        await page.locator('#name').fill('Deepika B U');
+        await page.locator('#saveBasicDetailsBtn').click();
+        await console.log("D's Naukri Login & update Successfull");
+        });
