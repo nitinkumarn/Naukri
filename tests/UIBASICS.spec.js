35ee8d23b4829079688f388b7745f87f29c6f513
Nitinkumar N <nitinkumarsuccess@gmail.com>
2025-11-05 15:31:45 +0530

    Added Naukri

--- tests/UIBASICS.spec.js
+++ tests/UIBASICS.spec.js
@@ -59,24 +59,24 @@ test('Naukri Login', async ({ browser }) => {
 //   await page.screenshot({ path: `screenshot-${formattedDate}.png`, fullPage: true });
 // });
 
-// test('Ds Naukri Login', async ({ browser }) => {
-//   const context = await browser.newContext();
-//   const page = await context.newPage();
-//   await page.goto("https://www.naukri.com/nlogin/login");
-//   await page.locator('#usernameField').fill("deepikabu1998@gmail.com");
-//   await page.locator('#passwordField').fill("DeepikaUmesh@098");
-//   await page.locator('button.waves-effect.waves-light.btn-large.btn-block.btn-bold.blue-btn.textTransform[type="submit"]').click();
-//   await page.locator('[href="/mnjuser/profile"]').click();
-//   await page.locator('[class="icon edit "]').click();
-//   await page.locator('#name').fill('Deepika B U');
-//   await page.locator('#saveBasicDetailsBtn').click();
-//   await console.log("D's Naukri Login & update Successful");
+test('Ds Naukri Login', async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://www.naukri.com/nlogin/login");
+  await page.locator('#usernameField').fill("deepikabu1998@gmail.com");
+  await page.locator('#passwordField').fill("DeepikaUmesh@098");
+  await page.locator('button.waves-effect.waves-light.btn-large.btn-block.btn-bold.blue-btn.textTransform[type="submit"]').click();
+  await page.locator('[href="/mnjuser/profile"]').click();
+  await page.locator('[class="icon edit "]').click();
+  await page.locator('#name').fill('Deepika B U');
+  await page.locator('#saveBasicDetailsBtn').click();
+  await console.log("D's Naukri Login & update Successful");
 
-//   // Capture full-page screenshot with date and time
-//   const date = new Date();
-//   const formattedDate = date.toISOString().replace(/:/g, '-'); // Replace colons to avoid issues in filenames
-//   await page.screenshot({ path: `screenshot-${formattedDate}.png`, fullPage: true });
-// });
+  // Capture full-page screenshot with date and time
+  const date = new Date();
+  const formattedDate = date.toISOString().replace(/:/g, '-'); // Replace colons to avoid issues in filenames
+  await page.screenshot({ path: `screenshot-${formattedDate}.png`, fullPage: true });
+});
 
 test('Naukri Login 2 ', async ({ browser }) => {
   const context = await browser.newContext();
